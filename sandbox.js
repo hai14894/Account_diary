5a8874d0d86bc4d9e27048a2e8ea209f4ce9da84
hai14894 <hohoanghai14894@gmail.com>
2021-03-17 15:13:49 +1100

    first commit

---
+++ sandbox.js
@@ -0,0 +1,5 @@
+console.log("hello");
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (element) {
+    console.log(element);
+});
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+console.log("hello");
+
+const inputs = document.querySelectorAll('input')
+
+inputs.forEach(element => {
+    console.log(element);
+    
+});
